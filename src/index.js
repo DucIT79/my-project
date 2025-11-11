ac91eb2264bb708e804acf2a034ef647ef368843
DucIT79 <ducddchemistry@gmail.com>
2025-11-11 19:43:41 +0700

    lastupdate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import store from './redux/Store';
-import { HashRouter } from 'react-router-dom';
+
 
 
 
@@ -14,9 +14,9 @@ root.render(
 
 
     <Provider store={store}>
-        <HashRouter>
+       
             <App />
-        </HashRouter>
+        
         
     </Provider>
 
